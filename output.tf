3995f6232808336cf4167a530298db86f111ef1f
Cristian Beraha <cristian@cloudops.works>
2024-08-23 21:05:57 -0300

    chore: outputs align

--- output.tf
+++ output.tf
@@ -5,11 +5,11 @@
 #
 
 output "lambda_exec_role_arn" {
-  value = try(var.lambda.iam.execRole.enabled, false) ? "" : aws_iam_role.lambda_exec[0].arn
+  value = try(var.lambda.iam.execRole.enabled, false) ? aws_iam_role.lambda_exec[0].arn : ""
 }
 
 output "lambda_exec_role" {
-  value = try(var.lambda.iam.execRole.enabled, false) ? "" : aws_iam_role.lambda_exec[0].name
+  value = try(var.lambda.iam.execRole.enabled, false) ? aws_iam_role.lambda_exec[0].name : ""
 }
 
 output "lambda_default_role_arn" {
@@ -21,18 +21,18 @@ output "lambda_default_role" {
 }
 
 output "lambda_function_role_arn" {
-  value = try(var.lambda.iam.enabled, false) ? "" : aws_iam_role.lambda_function[0].arn
+  value = try(var.lambda.iam.enabled, false) ? aws_iam_role.lambda_function[0].arn : ""
 }
 
 output "lambda_function_role" {
-  value = try(var.lambda.iam.enabled, false) ? "" : aws_iam_role.lambda_function[0].name
+  value = try(var.lambda.iam.enabled, false) ? aws_iam_role.lambda_function[0].name : ""
 }
 
-output "function_name_arn" {
+output "function_arn" {
   value = aws_lambda_function.lambda_function.arn
 }
 
-output "function_name_invoke_arn" {
+output "function_invoke_arn" {
   value = aws_lambda_function.lambda_function.invoke_arn
 }
 
