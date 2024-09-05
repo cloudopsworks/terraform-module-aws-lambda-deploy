623bf648bf910cd596af9fbf4623a0f72ef8be1c
Cristian Beraha <cristian@cloudops.works>
2024-09-05 17:28:37 -0300

    chore: added missing permissions

--- iam-role-exec.tf
+++ iam-role-exec.tf
@@ -63,7 +63,10 @@ data "aws_iam_policy_document" "lambda_exec_ec2" {
     actions = [
       "ec2:CreateNetworkInterface",
       "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeSubnets",
       "ec2:DeleteNetworkInterface",
+      "ec2:AssignPrivateIpAddresses",
+      "ec2:UnassignPrivateIpAddresses",
     ]
     resources = ["*"]
   }
