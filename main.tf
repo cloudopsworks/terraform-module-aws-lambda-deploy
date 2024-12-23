002a3fa3332e8295c808988fb1a36ba03277dfcc
Cristian Beraha <cristian@cloudops.works>
2024-12-23 11:16:16 -0300

    Remove unused module dependency from Lambda resource
    The `module.download_version` dependency was removed from the `depends_on` block as it is no longer required. This simplifies the resource configuration and avoids unnecessary coupling.

--- main.tf
+++ main.tf
@@ -76,7 +76,6 @@ resource "aws_lambda_function" "lambda_function" {
   depends_on = [
     aws_iam_role_policy_attachment.lambda_function_logs,
     aws_cloudwatch_log_group.lambda_function_logs,
-    module.download_version,
   ]
 }
 
