42bb4bb092d5b39e253d850ac96821caba31a90e
Cristian Beraha <cristian@cloudops.works>
2025-10-06 11:54:34 -0300

    PIP-28 Refactor security group egress rules into a separate resource and update `region_arr` to use `data.aws_region.current.id`.

--- sg.tf
+++ sg.tf
@@ -16,11 +16,20 @@ resource "aws_security_group" "this" {
   count  = try(var.lambda.vpc.create_security_group, false) && try(var.lambda.vpc.enabled, false) ? 1 : 0
   name   = "${var.release.name}-${var.namespace}-sg"
   vpc_id = data.aws_subnet.lambda_sub[0].vpc_id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = merge(local.release_tags, local.all_tags)
+  tags   = merge(local.release_tags, local.all_tags, {
+    "Name" = "${var.release.name}-${var.namespace}-sg"
+  })
+}
+
+resource "aws_security_group_rule" "egress" {
+  count             = try(var.lambda.vpc.create_security_group, false) && try(var.lambda.vpc.enabled, false) ? 1 : 0
+  description       = "Egress rule to allow all outbound traffic"
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.this[0].id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
 }
